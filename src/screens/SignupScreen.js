26d83f01ecae8159ed1fd17d8495d0d4aacf6645
J <igibliss00@gmail.com>
2019-08-26 16:26:23 -0400

    Added the Sign Up Screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useContext } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text } from 'react-native-elements'
+import { Context as AuthContext } from '../context/AuthContext'
+import AuthForm from '../components/AuthForm'
+
+import Spacer from '../components/Spacer'
+// import { TouchableOpacity } from '../../../../../../../Library/Caches/typescript/2.6/node_modules/@types/react-native';
 
 const SignupScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext)
+
     return (
-        <View>
-            <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-            <Button
-             title="Go to Signin"
-             onPress={() => navigation.navigate('Signin')}
+        <View style={styles.container}>
+            <AuthForm 
+                headerText='Sign Up for Tracker'
+                errorMessage={state.errorMessage}
+                submitButtonText='Sign Up'
+                onSubmit={signup}
             />
-            <Button
-             title="Go to Main Flow"
-             onPress={() => navigation.navigate('mainFlow')}
-            />
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>Already have an account? Sign in instead</Text>
+                </Spacer>
+            </TouchableOpacity>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+SignupScreen.navigationOptions = () => {
+    return {
+        header: null
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        flex: 1,
+        marginBottom: 200
+    },
+    link: {
+        color: 'blue',
+    }
+})
 
 export default SignupScreen
